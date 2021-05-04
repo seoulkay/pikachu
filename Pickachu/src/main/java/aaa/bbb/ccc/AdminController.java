6b0e1ea0e4fb7f08789416f7c9f39841c86e78aa
KHW <KHW@localhost>
2021-05-04 23:45:46 +0900

    워드클라우드 표현까지 커밋.. 금요일 숙제는 아직.

--- Pickachu/src/main/java/aaa/bbb/ccc/AdminController.java
+++ Pickachu/src/main/java/aaa/bbb/ccc/AdminController.java
@@ -6,11 +6,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-
-
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,6 +21,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -27,10 +31,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling;
 //import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,6 +44,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import aaa.bbb.ccc.entity.Member;
 import aaa.bbb.ccc.entity.PortalNews;
+import aaa.bbb.ccc.entity.PortalNews2;
 import aaa.bbb.ccc.entity.Post;
 import io.ipgeolocation.api.Geolocation;
 import io.ipgeolocation.api.GeolocationParams;
@@ -47,6 +53,7 @@ import aaa.bbb.ccc.entity.CountryData;
 import aaa.bbb.ccc.entity.JsoupReply;
 import aaa.bbb.ccc.entity.LoginCount;
 import aaa.bbb.ccc.entity.LoginLog;
+import aaa.bbb.ccc.entity.Top20Json;
 //import aaa.bbb.ccc.Scheduler;
 
 @Controller
@@ -1043,11 +1050,13 @@ public class AdminController {
 	public @ResponseBody void whyNoSchedule() {
 		//jsoupTest0();
 		//newsListToDB(jsoupNaverHead());
+		getNaver_newsHeadline();
+		getDaum_newsHeadline();			
 	}
 	
 
 	// 네이버 뉴스 가져오는 함수 
-	public static  void getNaver_newHaedline() {
+	public static  void getNaver_newsHeadline() {
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println("네이버 뉴스 긁어올게 " +dateFormat.format(calendar.getTime())+" 기다려  ");
@@ -1056,24 +1065,56 @@ public class AdminController {
 			Document doc = Jsoup.connect("https://news.naver.com/main/home.nhn").get();
 			System.out.println(doc.title());
 			Elements newsHeadlines = doc.select("ul[class=hdline_article_list]").select("li");
-			newsTitle toDay = new newsTitle();
+			PortalNews toDay = new PortalNews();
 					
 					
-		for(Element elem : newsHeadlines) {
-				toDay.setTitle(elem.select("li").text());
+			for(Element elem : newsHeadlines) {
+				toDay.setDescription(elem.select("li").text());
 				toDay.setLink("https://news.naver.com/"+elem.select("div[class=hdline_article_tit] a").attr("href"));
-				toDay.setSource("NAVER");
-						
-				insertNews(toDay);
-						
-				}
-
-					}catch(IOException e) {
-						e.printStackTrace();
-					}
+				toDay.setSource("NAVER");				
+				insertNews(toDay);						
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 		
-				}
+	}
 	
+	//다음헤드라인 뉴스 가져오는 함수 : 5개만 가져오기 
+	public static void getDaum_newsHeadline() {
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("다음 뉴스 긁어올게 " +dateFormat.format(calendar.getTime())+" 기다려  ");
+		
+		try {
+		Document doc = Jsoup.connect("https://www.daum.net/").get();
+		System.out.println(doc.title());
+		Elements newsHeadlines = doc.select("ul[class=list_txt]").select("li");
+		PortalNews toDay = new PortalNews();
+		int i = 0;
+		
+			for(Element elem : newsHeadlines) {
+				toDay.setDescription(elem.select("li").text());
+				toDay.setLink(elem.select("a").attr("href"));
+				toDay.setSource("DAUM");
+				i += 1 ;
+				System.out.println(toDay.getLink());
+				
+				insertNews(toDay);
+				System.out.println(i+"번째 뉴스 기록중");
+				if(i == 5) {
+					System.out.println("멈출게 5라서");
+					break;
+				}
+			}
+
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	//뉴스를 하나씩 디비에 넣는 함수. 뉴스를 가져오는 네이버, 다음 함수 안에서 쓰여진다.
 	public static void insertNews(PortalNews p1){
 		
 		String resource = "aaa/bbb/ccc/mybatis_config.xml";
@@ -1095,6 +1136,484 @@ public class AdminController {
 				e.printStackTrace();
 		}
 	}
+	
+	
+	@RequestMapping(value = "admin/news24", method = RequestMethod.GET)
+	public @ResponseBody void news24() {
+		//List<PortalNews2> result = getTodayData();
+		List<PortalNews2> resultNaver = getTodayDataNaver();
+		List<PortalNews2> resultDaum = getTodayDataDaum();
+		//System.out.println("가져온 데이터는 "+result);
+		//Map<String, Integer> wordsMap = splitWords(result);
+		Map<String, Integer> wordsMapNaver = splitWords(resultNaver);
+		Map<String, Integer> wordsMapDaum = splitWords(resultDaum);
+		//System.out.println("쪼개서 가져온 단어들과 등장횟수는 : "+wordsMap);
+		
+		List <String> top20Naver = top20(wordsMapNaver);
+		List <String> top20Daum = top20(wordsMapDaum);
+		
+		//배열을 하나의 스트링으로 바꿔준다. 
+		String stringResultNaver = String.join(" ",top20Naver);
+		String stringResultDaum = String.join(" ",top20Daum);
+		
+		insertTop20(stringResultNaver, "NAVER");
+		insertTop20(stringResultDaum, "DAUM");
+		
+		
+		//top20SplitToMap(selectLast20("naver"));
+		//top20SplitToMap(selectLast20("daum"));
+		
+		//List <PortalNews2> DbTop20Naver = getTop20Naver();
+//		System.out.println("리스트 네이버 1위: "+DbTop20Naver.get(0).getTitle());
+		//List <PortalNews2> DbTop20Daum = getTop20Daum();;
+//		System.out.println("리스트 다음 1위: "+DbTop20Daum.get(0).getTitle());
+		//System.out.println(reSplitWords(DbTop20Naver));
+	}
+	
+	//db에서 다시 가져온 리스트를 다시 쪼개서 맵에 넣는다.
+	
+
+
+	
+	//4월 14일 숙제
+	//디비에 들어간 뉴스20 최근 자료를 가져와서 다시 맥으로 출력하는 로직 시작
+	
+	//포탈 파라미터 받아서 24시간 마지막 타이틀 한줄 가져오기	
+
+
+	
+	
+	
+	
+	
+	//일단 매핑을 두개로 나눠서 하드코딩부터 했음.
+	
+//	@RequestMapping(value = "admin/news24/naver", method = RequestMethod.GET)
+//	@ResponseBody
+//	public JSONObject news24Naver() {
+//		//List<PortalNews2> result = getTodayData();
+//		List<PortalNews2> resultNaver = getTodayDataNaver();
+//		//System.out.println("가져온 데이터는 "+result);
+//		//Map<String, Integer> wordsMap = splitWords(result);
+//		Map<String, Integer> wordsMapNaver = splitWords(resultNaver);
+//		//System.out.println("쪼개서 가져온 단어들과 등장횟수는 : "+wordsMap);
+//		List <String> top20Naver = top20(wordsMapNaver);
+//
+//		//배열을 하나의 스트링으로 바꿔준다. 
+//		String stringResultNaver = String.join(" ",top20Naver);
+//		
+//		insertTop20(stringResultNaver, "NAVER");
+//		
+//		
+//		//리턴된 JSON오브젝트를 result에 집어넣었음..
+//		JSONObject result = top20SplitToMap(selectLast20("naver"));		
+//		return result;
+//	}
+//	
+//	@RequestMapping(value = "admin/news24/daum", method = RequestMethod.GET)
+//	@ResponseBody
+//	public JSONObject news24Daum() {
+//
+//		List<PortalNews2> resultDaum = getTodayDataDaum();
+//
+//		Map<String, Integer> wordsMapDaum = splitWords(resultDaum);
+//		//System.out.println("쪼개서 가져온 단어들과 등장횟수는 : "+wordsMap);
+//
+//		List <String> top20Daum = top20(wordsMapDaum);
+//		
+//		//배열을 하나의 스트링으로 바꿔준다. 
+//
+//		String stringResultDaum = String.join(" ",top20Daum);		
+//
+//		insertTop20(stringResultDaum, "DAUM");
+//		
+//		JSONObject result = top20SplitToMap(selectLast20("daum"));		
+//		return result;
+//	}
+
+	
+	@RequestMapping(value = "/get/todayTop20/{id}", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public List<Top20Json> getFoosBySimplePathWithPathVariable(
+	  @PathVariable String id) {
+		List<Top20Json> top20Json = new ArrayList<Top20Json>();
+		
+		//ObjectMapper mapper = new ObjectMapper();
+	
+		try {
+			PortalNews2 sourceIs = new PortalNews2();
+			sourceIs.setSource(id);
+			//jsonList = mapper.writeValueAsString(orrm2(top20SplitToMap(selectLast20(sourceIs))));
+			top20Json=orrm2(top20SplitToMap(selectLast20(sourceIs)));
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return top20Json;
+		
+	}
+	
+	@RequestMapping(value = "admin/index5", method = RequestMethod.GET)
+	public String adminIndex5(Locale locale, Model model, String source) {
+		
+		PortalNews2 sourceIs = new PortalNews2();
+		sourceIs.setSource(source);
+		String result = source;
+		String url = newsSearchUrl(source);
+		
+		model.addAttribute("url", url);
+		model.addAttribute("source", result);
+		return "admin/index5";
+	}
+	
+	@RequestMapping(value = "/get/news20", method = {RequestMethod.GET})
+	public @ResponseBody List<Top20Json> news20(@RequestParam("data") String data){
+	
+		//ObjectMapper mapper = new ObjectMapper();
+		List<Top20Json> jsonList = new ArrayList<Top20Json>();
+//		String jsonList="";
+		
+		PortalNews2 sourceIs = new PortalNews2();
+		sourceIs.setSource(data);
+		jsonList = orrm2(top20SplitToMap(selectLast20(sourceIs)));
+//		System.out.println("news20Ajax 시작 : "+jsonList);
+	
+	    return jsonList;
+	}
+	
+	
+	
+	public static String newsSearchUrl(String source) {
+		String result = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=";
+		String str = "daum";
+		
+		if (source.equals(str)) {
+			result = "https://search.daum.net/search?w=tot&DA=23A&rtmaxcoll=NNS&q=";
+		}
+		
+		return result;
+	}
+	
+	
+	//맵을 제이슨으로 바꾸는 함수인데... 
+	//JSONObject json = new JSONObject(result); 이렇게 하는 것과 어떤 차이가 있는지 질문. 
+	
+	public static JSONObject convertMapToJson(Map<String, Integer> map) {
+		JSONObject json = new JSONObject();
+		
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			json.put(key, value);
+		}
+		
+		return json;
+	}
+	
+
+	public static List<Top20Json> orrm2(Map<String,Integer> p1){
+		
+		List<Top20Json> result = new ArrayList<Top20Json>();
+			
+		//맵을 순서가 있는 리스트로 바꿀 때 entrySet 함수사용. 
+		//멥을 객체로 가지고 있는 리스트
+		//키랑 벨류를 호출하면 나오게 된다.		
+		List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(p1.entrySet());
+		
+		
+		System.out.println("정렬을 시작해볼까");
+		//자료구조의 값을 비교해서 정렬하는 기술  
+		Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
+			// compare로 값을 비교
+			public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+				// 오름 차순 정렬
+				return obj2.getValue().compareTo(obj1.getValue());
+			}
+		});
+		
+		
+		//리스트 앤트리 안에 들어있는 객체의 키와 밸류를 리절트 리스트 안에 반복해서 넣어준다.
+		int i = 0;
+		for(Entry<String, Integer> entry : list_entries) {
+			System.out.println(i+1 + "위 : " +entry.getKey() + " : " + entry.getValue());
+			Top20Json temp = new Top20Json();
+			temp.setText(entry.getKey());
+			temp.setSize(entry.getValue());
+			result.add(temp);
+			
+			i += 1;
+			
+			System.out.println(result);
+			if(i == 20) {
+				break;
+			}
+	
+		}
+		
+		return result;
+	}
+	
+	
+	
+	//가져온 타이틀을 
+	//콤마로 자르고
+	//:를 기준으로 키값, 벨류값으로 나눠서 맵에 넣기
+	
+	public static Map<String, Integer> top20SplitToMap(PortalNews2 p1){		
+		
+		//비어있는 맵을 만든다. 
+		Map<String, Integer> result = new HashMap<String, Integer>();		
+
+		String top20[] = p1.getTop20().trim().split(",");
+		//trim으로 앞뒤여백 제거..
+		//어레이 top20는 0번지 '남양유업:40' 1번지 '대통령:28' ...
+		
+		for(int i=0;i<top20.length;i++) {
+			String temp[] =	top20[i].split(":");			
+			result.put(temp[0], Integer.parseInt(temp[1]));	
+			//System.out.println(i+"번째로 맵에 들어가는 중"+result);
+			//:로 잘라. result = temp 어레이 0번지 '남양유업' 1번지 '40' 
+			
+		}	
+		
+		  //최종 완성될 JSONObject 선언(전체)
+//        JSONObject jsonObject = new JSONObject();
+// 
+//        //top20의 JSON정보를 담을 Array 선언
+//        JSONArray top20Array = new JSONArray();
+// 
+//        //top20의 키워드 1개 정보가 들어갈 JSONObject 선언
+//        JSONObject newsInfo = new JSONObject();
+			
+          //정보 입력
+//        newsInfo.put("title", temp[0]);
+//        newsInfo.put("value",Integer.parseInt(temp[1]));
+//        //Array에 입력
+//        top20Array.add(newsInfo);
+//        
+//        //전체의 JSONObject에 사람이란 name으로 JSON의 정보로 구성된 Array의 value를 입력
+//        jsonObject.put("top20", top20Array);
+        
+		System.out.println("맵에 다시 들어간 결과는: "+result);
+		
+		//System.out.println("json array 만든 결과는: "+jsonObject);
+		        
+
+        
+		return result;
+	}
+	
+	
+	
+	
+	public static PortalNews2 selectLast20(PortalNews2 p1){
+		String resource = "aaa/bbb/ccc/mybatis_config.xml";
+		InputStream inputStream;
+		PortalNews2 result = new PortalNews2();
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			result = session.selectOne("aaa.bbb.ccc.BaseMapper.selectLast20", p1);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("디비에서 "+p1.getSource()+" 최근것 가져왔다. "+"\n"+result.getTop20());
+		return result;
+	}
+		
+	
+	
+	public static void insertTop20(String p1, String p2){
+		PortalNews2 top20 = new PortalNews2();
+		top20.setTop20(p1);
+		top20.setSource(p2);
+		
+		String resource = "aaa/bbb/ccc/mybatis_config.xml";
+		InputStream inputStream;
+		
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			session.insert("aaa.bbb.ccc.BaseMapper.insertTop20", top20);
+			
+			session.commit();
+			session.close();
+			//System.out.println("디비에서 긁어온24 뉴스 : "+result);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	
+	
+//	4월 6일 숙제	//	
+//	1. 지금시간 -24시간 한 데이터만 수집 - DB에서 가지고 온다. 960개
+		//select24News(); 함수 만들기
+			// 현재시각 넣어줘야하고, 마이바티스에 sql 노테이션 찾아서 쓰기.
+			// 검증하는 것 생각하기. 갯수가 맞는지 또는...
+		// 자바 포탈 뉴스 객체 배열에 집어넣는다. - 중복이 존재하는 리스트 A
+	public static List<PortalNews2> getTodayData(){
+		String resource = "aaa/bbb/ccc/mybatis_config.xml";
+		InputStream inputStream;
+		List<PortalNews2> result = new ArrayList<PortalNews2>();
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			result = session.selectList("aaa.bbb.ccc.BaseMapper.selectTodayNews");
+			//System.out.println("디비에서 긁어온24 뉴스 : "+result);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static List<PortalNews2> getTodayDataNaver(){
+		String resource = "aaa/bbb/ccc/mybatis_config.xml";
+		InputStream inputStream;
+		List<PortalNews2> result = new ArrayList<PortalNews2>();
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			result = session.selectList("aaa.bbb.ccc.BaseMapper.selectTodayNewsNaver");
+			//System.out.println("디비에서 긁어온24 뉴스 : "+result);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static List<PortalNews2> getTodayDataDaum(){
+		String resource = "aaa/bbb/ccc/mybatis_config.xml";
+		InputStream inputStream;
+		List<PortalNews2> result = new ArrayList<PortalNews2>();
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			SqlSession session = sqlSessionFactory.openSession();
+			result = session.selectList("aaa.bbb.ccc.BaseMapper.selectTodayNewsDaum");
+			//System.out.println("디비에서 긁어온24 뉴스 : "+result);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+//	2. url이 같은것은 제외한다.
+		// duplDel(); 함수 만들기 
+			// 어떻게 해야할까?? 
+			// 리스트 B
+			// for(int i=0; A.size(); i++{
+			// 		if(!
+		
+//	3. 타이틀을 스페이스로 자른다.
+//	4. 자른 스트링을 맵에 넣는다. - 맵: 키값으로 불러온다, 배열:주소(순서)불러온다.
+//	5. 맵을 업데이트 한다.
+	public static Map<String, Integer> splitWords(List<PortalNews2> p1){
+		Map<String, Integer> result = new HashMap<String, Integer>();
+		
+		for(int i=0; i<p1.size();i++) {
+			String newsTitle = p1.get(i).getTitle();
+			//System.out.println(newsDesc);
+			
+			String piece[] = newsTitle.split(" ");
+			
+			for(int j=0;j<piece.length;j++) {
+				String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
+				piece[j] =piece[j].replaceAll(match, "");
+				
+				if(piece[j].equals("관련기사")||piece[j].equals("개수")||piece[j].equals("")) {	
+					System.out.println("단어 뺄게");
+				}else {			
+					System.out.println(piece[j]+" 단어 저장중");
+					if(result.containsKey(piece[j])) {
+						result.put(piece[j], result.get(piece[j])+1);
+						//System.out.println("중복단어 갯수 : "+piece[j]+1);
+						
+					}else {
+						result.put(piece[j], 1);
+					}									
+				}
+			}
+		}
+		System.out.println(result+"\n"+"생성했습니다.");
+		return result;
+	}
+	
+//	6. 맵에서 상위 20개만 정렬하여 System.out.print 한다.
+	//top20봅는 함수
+	public static List<String> top20(Map<String,Integer> p1) {
+		//리절트는 앰티에서 출발 
+		List<String> result = new ArrayList<String>();
+		
+		//맵을 앤트리셋으로 바꾼다. 
+		//정렬이 안된 상태 
+		List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(p1.entrySet());
+		
+		//자료구조(리스트, 맵....)를 구현 Collections, 인자로 엔트리라는 객체를 받음... 
+		//sort 라는 함수를 자주 쓴다. 아래는 쓰는 방식.
+		//맵을 넣으면 정렬해서 리스트로 뽑아준다. 
+		Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
+			// compare로 값을 비교
+			public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
+				// 오름 차순 정렬
+				return obj2.getValue().compareTo(obj1.getValue());
+			}
+		});
+		//list_entries는 정렬이 끝남 
+		
+			System.out.println("오름 차순 정렬");
+
+			// 결과 출력
+			
+			int i = 0; //0에서 시작 
+			
+			for(Entry<String, Integer> entry : list_entries) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+				
+				//한번에 1씩 증가 
+				if(i<19) {
+					result.add(entry.getKey() + ":" + entry.getValue()+",");				
+				}else {
+					//19번에서 일어나는 일 
+					result.add(entry.getKey() + ":" + entry.getValue());
+				}
+				i += 1;
+				//20이 되었다.
+				
+				//20번 돌면 그만 돌아라.
+				if(i == 20) {					
+					break;
+				}
+				// 리절트를 리턴하는데... 리턴값에 뭔가를 넣어준듯.
+				
+				// 정렬된 list_entries 를 리절트에 넣어줌				
+				//result.addAll(list_entries);
+				
+			}
+		
+		//
+		
+		
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
